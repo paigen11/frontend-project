c0e9139dbc355555f64bb8f25556be2a03b655e9
paigen11 <paigen11@gmail.com>
2016-08-23 17:03:58 -0400

    Updated where compass directory points

--- compass/config.rb
+++ compass/config.rb
@@ -2,7 +2,7 @@
 
 #Folder settings
 relative_assets = true      		#because we're not working from the root
-css_dir = "../public/stylesheets"	#where the CSS will saved
+css_dir = "../css"	#where the CSS will saved
 sass_dir = "sass"           		#where our .scss files are
 images_dir = "../public/images"    	#the folder with your images
 
