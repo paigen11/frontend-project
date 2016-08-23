685561dd514ffd6005f5f12b1d5e784d6faeaabf
PirieD704 <dp75975@gmail.com>
2016-08-23 10:10:20 -0400

    moved generate markers into generator

--- js/functions/generator.js
+++ js/functions/generator.js
@@ -10,6 +10,26 @@
 //     [-84.1703796387,33.9775311374]  // Northeast coordinates
 var point= [33.5471175795, -84.549407959];
 
+function generateMarkers() {
+    var tempPoint = generate(point[0], point[1], .4);
+    marker = L.marker(tempPoint, {icon: myIcon})
+        .addTo(map)
+        .on("click", function(e){
+            map.removeLayer(this);
+            //remove element from markerList array
+            var index = markerList.indexOf(this);
+            if(index > -1){
+                markerList.splice(index, 1);
+            }
+            scoreboard.innerHTML++;
+        })
+    //push marker coords to an array    
+    markerList.push(marker);
+    //if markerList is longer than X show test - this will eventually end the game when the player's overrun with zombies
+    if(markerList.length > 10){
+        console.log("test, test");
+    }
+}   
 
 function generate(south, west, interval){
 	//randomly generates a coordinate based on the interval passed for west
--- js/functions/menu.js
+++ js/functions/menu.js
@@ -1,23 +1,3 @@
 var count = 0;
 var scoreboard = document.getElementById('scoreboard');
 scoreboard.innerHTML = count;
-function generateMarkers() {
-    var tempPoint = generate(point[0], point[1], .4);
-    marker = L.marker(tempPoint, {icon: myIcon})
-        .addTo(map)
-        .on("click", function(e){
-            map.removeLayer(this);
-            //remove element from markerList array
-            var index = markerList.indexOf(this);
-            if(index > -1){
-                markerList.splice(index, 1);
-            }
-            scoreboard.innerHTML++;
-        })
-    //push marker coords to an array    
-    markerList.push(marker);
-    //if markerList is longer than X show test - this will eventually end the game when the player's overrun with zombies
-    if(markerList.length > 10){
-        console.log("test, test");
-    }
-}   
