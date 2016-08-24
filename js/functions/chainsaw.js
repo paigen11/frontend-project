702c003bfefdc49999fe104d79d1b955b1493289
Eric Ettensohn <ericettensohn@gmail.com>
2016-08-24 10:21:40 -0400

    Started message framework, added chainsaw.js comments.

--- js/functions/chainsaw.js
+++ js/functions/chainsaw.js
@@ -1,9 +1,16 @@
+// chainsawAvailable is set to true on a timer and will be used together with a CSS class to animate the bomb button. 
+// chainsawSelected can't be set as true unless bombAvailable is true
 var chainsawAvailable = false;
+// chainsawSelected  is set to true on a click event, and will unlock the bomb weapon
 var chainsawSelected = false;
 
+
+// call chainsawDelay once to start weapon timer
 chainsawDelay();
 
+// click listener for weapon button
 $('.link_two').on('click', function() {
+	// only allow weapon to be selected if it is available
 	if(chainsawAvailable){
 		chainsawSelected = true;
 		$('.link_two').removeClass('bomb-available');
@@ -12,6 +19,8 @@ $('.link_two').on('click', function() {
 	
 })
 
+// sawed is called by a mouseover listener placed on all markers
+// if the weapon is available and listener fires, the marker is removed
 function sawed(marker){
 	if(chainsawSelected){
 		map.removeLayer(marker);
