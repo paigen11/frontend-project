1bf63ac6817a20758aa71c3bc1a7cb97f1864be7
PirieD704 <dp75975@gmail.com>
2016-09-06 17:31:55 -0400

    more changes

--- scripts/functions/explosion-universal.js
+++ scripts/functions/explosion-universal.js
@@ -1,62 +1,62 @@
 function explosionUniversal(marker) {
 	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_1.png', iconSize: [96, 96]});
-		marker.setIcon(newIcon)
-	}, 100);
-	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_2.png', iconSize: [96, 96]});
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_1.png', iconSize: [96, 96]});
 		marker.setIcon(newIcon)
 	}, 200);
 	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_3.png', iconSize: [96, 96]});
-		marker.setIcon(newIcon)
-	}, 300);
-	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_4.png', iconSize: [96, 96]});
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_2.png', iconSize: [96, 96]});
 		marker.setIcon(newIcon)
 	}, 400);
 	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_5.png', iconSize: [96, 96]});
-		marker.setIcon(newIcon)
-	}, 500);
-	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_6.png', iconSize: [96, 96]});
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_3.png', iconSize: [96, 96]});
 		marker.setIcon(newIcon)
 	}, 600);
 	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_7.png', iconSize: [96, 96]});
-		marker.setIcon(newIcon)
-	}, 700);
-	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_8.png', iconSize: [96, 96]});
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_4.png', iconSize: [96, 96]});
 		marker.setIcon(newIcon)
 	}, 800);
 	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_9.png', iconSize: [96, 96]});
-		marker.setIcon(newIcon)
-	}, 900);
-	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_10.png', iconSize: [96, 96]});
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_5.png', iconSize: [96, 96]});
 		marker.setIcon(newIcon)
 	}, 1000);
 	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_11.png', iconSize: [96, 96]});
-		marker.setIcon(newIcon)
-	}, 1100);
-	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_12.png', iconSize: [96, 96]});
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_6.png', iconSize: [96, 96]});
 		marker.setIcon(newIcon)
 	}, 1200);
 	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_13.png', iconSize: [96, 96]});
-		marker.setIcon(newIcon)
-	}, 1300);
-	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_14.png', iconSize: [96, 96]});
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_7.png', iconSize: [96, 96]});
 		marker.setIcon(newIcon)
 	}, 1400);
 	setTimeout(function(){
-		newIcon = L.icon({iconUrl: 'img2/explosion_15.png', iconSize: [96, 96]});
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_8.png', iconSize: [96, 96]});
 		marker.setIcon(newIcon)
-	}, 1500);
+	}, 1600);
+	setTimeout(function(){
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_9.png', iconSize: [96, 96]});
+		marker.setIcon(newIcon)
+	}, 1800);
+	setTimeout(function(){
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_10.png', iconSize: [96, 96]});
+		marker.setIcon(newIcon)
+	}, 2000);
+	setTimeout(function(){
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_11.png', iconSize: [96, 96]});
+		marker.setIcon(newIcon)
+	}, 2200);
+	setTimeout(function(){
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_12.png', iconSize: [96, 96]});
+		marker.setIcon(newIcon)
+	}, 2400);
+	setTimeout(function(){
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_13.png', iconSize: [96, 96]});
+		marker.setIcon(newIcon)
+	}, 2600);
+	setTimeout(function(){
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_14.png', iconSize: [96, 96]});
+		marker.setIcon(newIcon)
+	}, 2800);
+	setTimeout(function(){
+		newIcon = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_15.png', iconSize: [96, 96]});
+		marker.setIcon(newIcon)
+	}, 3000);
 }
