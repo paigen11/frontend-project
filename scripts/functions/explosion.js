8a1c33f0a5626df598960ec8ecbbc6fd60f08d6c
PirieD704 <dp75975@gmail.com>
2016-09-06 17:38:46 -0400

    submit

--- scripts/functions/explosion.js
+++ scripts/functions/explosion.js
@@ -3,61 +3,61 @@ function explosion(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_1.png', iconSize: [96, 96]});
 		home.setIcon(castle)
 		home.setZIndexOffset(1000);
-	}, 100);
+	}, 200);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_2.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 200);
+	}, 400);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_3.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 300);
+	}, 600);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_4.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 400);
+	}, 800);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_5.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 500);
+	}, 1000);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_6.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 600);
+	}, 1200);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_7.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 700);
+	}, 1400);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_8.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 800);
+	}, 1600);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_9.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 900);
+	}, 1800);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_10.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 1000);
+	}, 2000);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_11.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 1100);
+	}, 2200);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_12.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 1200);
+	}, 2400);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_13.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 1300);
+	}, 2600);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_14.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 1400);
+	}, 2800);
 	setTimeout(function(){
 		castle = L.icon({iconUrl: 'http://davidapirie.com/zombies/img2/explosion_15.png', iconSize: [96, 96]});
 		home.setIcon(castle)
-	}, 1500);
+	}, 3000);
 }
