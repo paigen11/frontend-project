dec1e4438bea4e93d236df02e9c16a7830f17053
Paige Niedringhaus <paigen11@gmail.com>
2016-09-07 11:54:45 -0400

    Update message.js

--- scripts/functions/message.js
+++ scripts/functions/message.js
@@ -3,12 +3,12 @@ function newMessage(message) {
 	$('#message').addClass('slide-in');
 	setTimeout(function(){
 		$('#message').removeClass('slide-in');
-	}, 4000)
+	}, 7000)
 }
 function endMessage(message) {
 	$('.end-message-text').text(message)
 	$('#end-message').addClass('slide-down');
 	setTimeout(function(){
 		$('#end-message').removeClass('slide-down');
-	}, 4000)
+	}, 5000)
 }
